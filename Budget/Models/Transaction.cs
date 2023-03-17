30a07d1070f928dc2f2f0670aadb0e535c1c9e60
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-03-18 02:21:46 +0300

    Change model properties

--- Budget/Models/Transaction.cs
+++ Budget/Models/Transaction.cs
@@ -1,17 +1,18 @@
+using System.ComponentModel.DataAnnotations;
 using Microsoft.EntityFrameworkCore;
 
 namespace Budget.Models;
 
 public class Transaction
 {
-    public int TransactionId { get; set; }
-    public required string TransactionName { get; set; }
-    public string? TransactionDescription { get; set; }
-    public Month Month { get; set; }
-    public DateTime TransactionDate { get; set; }
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public string? Description { get; set; }
+    public Month? Month { get; set; }
+    public DateTime? Date { get; set; }
     
     [Precision(10, 2)]
-    public decimal Cost { get; set; }
+    public decimal? Cost { get; set; }
 
     public int CategoryId { get; set; }
     public Category? Category { get; set; }
