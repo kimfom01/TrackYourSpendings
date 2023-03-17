32937aeaa568f3d320e7266f64870fac41682b9f
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-03-18 02:22:07 +0300

    Add post request for add transaction

--- Budget/wwwroot/js/site.js
+++ Budget/wwwroot/js/site.js
@@ -1,16 +1,15 @@
-﻿let uri = '/addWallet'
+﻿"use strict";
+
+const addWallet = async () => {
+    let uri = '/Home/AddWallet'
     
-addWallet = async (e) => {
-    e.preventDefault();
+    const name = document.getElementById('walletName').value;
+    const income = document.getElementById('walletIncome').value;
     
-    // const form = document.querySelector('form');
-    console.log(e.target);
-
-    await new Promise(res => setTimeout(res, 60000));
-
-    const wallet = new FormData(form);
-
-    console.log(JSON.stringify(wallet));
+    const wallet = {
+        name,
+        income
+    }
 
     fetch(uri, {
         method: 'post',
@@ -22,6 +21,33 @@ addWallet = async (e) => {
     })
         .then(res => console.log(res.status))
         .catch(err => console.error(err));
+};
 
-    // window.location.href = uri;
-}
+const addTransaction = async () => {
+    const uri = '/Home/AddTransaction';
+    
+    const name = document.getElementById("transactionName").value;
+    const description = document.getElementById("transactionDescription").value;
+    const month = document.getElementById("transactionMonth").value;
+    const cost = document.getElementById("transactionCost").value;
+    const categoryId = document.getElementById("categoryId").value;
+    
+    const transaction = {
+        name,
+        description,
+        month,
+        cost,
+        categoryId
+    }
+
+    fetch(uri, {
+        method: 'post',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(transaction)
+    })
+        .then(res => console.log(res.status))
+        .catch(err => console.error(err));
+};
