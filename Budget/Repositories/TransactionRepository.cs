c11a83db5a2ef0cb7990bc01c15dddde0917beab
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-03-24 17:47:32 +0300

    Implement GetTransactionsWithCategories

--- Budget/Repositories/TransactionRepository.cs
+++ Budget/Repositories/TransactionRepository.cs
@@ -1,5 +1,7 @@
+using System.Linq.Expressions;
 using Budget.Context;
 using Budget.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Budget.Repositories;
 
@@ -7,5 +9,14 @@ public class TransactionRepository : Repository<Transaction>, ITransactionReposi
 {
     public TransactionRepository(BudgetDbContext budgetDbContext) : base(budgetDbContext)
     {
+    }
+
+    public async Task<IEnumerable<Transaction>?> GetTransactionsWithCategories(Expression<Func<Transaction, bool>> predicate)
+    {
+        var transactionsWithCategories = DbEntitySet
+            .Where(predicate)
+            .Include(tr => tr.Category);
+
+        return await transactionsWithCategories.ToListAsync();
     }
 }
