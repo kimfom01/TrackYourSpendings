43762861d82a4deead7ad81de320b5d3ec55705e
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-03-28 15:07:56 +0300

    Use js to extract data from row and pass to modal

--- Budget/wwwroot/js/site.js
+++ Budget/wwwroot/js/site.js
@@ -2,10 +2,10 @@
 
 const addWallet = async () => {
     let uri = '/Home/AddWallet'
-    
+
     const name = document.getElementById('walletName').value;
     const income = document.getElementById('walletIncome').value;
-    
+
     const wallet = {
         name,
         income
@@ -25,13 +25,13 @@ const addWallet = async () => {
 
 const addTransaction = async () => {
     const uri = '/Home/AddTransaction';
-    
+
     const name = document.getElementById("transactionName").value;
     const description = document.getElementById("transactionDescription").value;
     const month = document.getElementById("transactionMonth").value;
     const cost = document.getElementById("transactionCost").value;
     const categoryId = document.getElementById("categoryId").value;
-    
+
     const transaction = {
         name,
         description,
@@ -51,3 +51,41 @@ const addTransaction = async () => {
         .then(res => console.log(res.status))
         .catch(err => console.error(err));
 };
+
+const updateTransaction = (transaction) => {
+    const updateId = document.getElementById("updateTransactionId");
+    const updateName = document.getElementById("updateTransactionName");
+    const updateDescription = document.getElementById("updateTransactionDescription");
+    const updateCost = document.getElementById("updateTransactionCost");
+    const updateMonth = document.getElementById("updateTransactionMonth");
+    const updateCategoryId = document.getElementById("updateTransactionCategoryId");
+    updateId.value = transaction.id;
+    updateName.value = transaction.name;
+    updateDescription.value = transaction.description;
+    updateCost.value = transaction.cost;
+    updateMonth.value = transaction.month;
+    updateCategoryId.value = transaction.categoryId;
+}
+
+const buttons = document.querySelectorAll("table button");
+
+buttons.forEach(btn => btn.addEventListener("click", (e) => {
+    e.preventDefault();
+    
+    const row = e.target.closest("tr");
+    
+    const transactionRow = row.children;
+    
+    const transaction = {
+        id: transactionRow[0].innerText,
+        name: transactionRow[1].innerText,
+        description: transactionRow[2].innerText,
+        cost: transactionRow[5].innerText,
+        month: transactionRow[6].innerText,
+        categoryId: transactionRow[7].innerText
+    };
+
+    console.table(transaction)
+    
+    updateTransaction(transaction)
+}))
