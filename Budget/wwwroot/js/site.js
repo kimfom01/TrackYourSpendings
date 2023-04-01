42b342cc4bd0a9950c6f121f994218cca559bbc5
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-04-01 20:47:34 +0300

    Clean up unused javascript code

--- Budget/wwwroot/js/site.js
+++ Budget/wwwroot/js/site.js
@@ -1,57 +1,5 @@
 ﻿"use strict";
 
-const addWallet = async () => {
-    let uri = '/Home/AddWallet'
-
-    const name = document.getElementById('walletName').value;
-    const income = document.getElementById('walletIncome').value;
-
-    const wallet = {
-        name,
-        income
-    }
-
-    fetch(uri, {
-        method: 'post',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(wallet)
-    })
-        .then(res => console.log(res.status))
-        .catch(err => console.error(err));
-};
-
-const addTransaction = async () => {
-    const uri = '/Home/AddTransaction';
-
-    const name = document.getElementById("transactionName").value;
-    const description = document.getElementById("transactionDescription").value;
-    const month = document.getElementById("transactionMonth").value;
-    const cost = document.getElementById("transactionCost").value;
-    const categoryId = document.getElementById("categoryId").value;
-
-    const transaction = {
-        name,
-        description,
-        month,
-        cost,
-        categoryId
-    }
-
-    fetch(uri, {
-        method: 'post',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(transaction)
-    })
-        .then(res => console.log(res.status))
-        .catch(err => console.error(err));
-};
-
 const populateModal = (transaction, action) => {
     const id = document.getElementById(`${action}TransactionId`);
     const name = document.getElementById(`${action}TransactionName`);
@@ -60,6 +8,7 @@ const populateModal = (transaction, action) => {
     const month = document.getElementById(`${action}TransactionMonth`);
     const categoryId = document.getElementById(`${action}TransactionCategoryId`);
     const walletId = document.getElementById(`${action}TransactionWalletId`);
+
     id.value = transaction.id;
     name.value = transaction.name;
     description.value = transaction.description;
