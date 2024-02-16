9efa5569acd97b3178a1fc5094dbd335fb3d7473
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2024-02-16 15:49:45 +0300

    Remove category reference

--- Web/Models/ApplicationUser.cs
+++ Web/Models/ApplicationUser.cs
@@ -5,6 +5,5 @@ namespace Web.Models;
 public class ApplicationUser : IdentityUser
 {
     public IEnumerable<Transaction>? Transactions { get; set; }
-    public IEnumerable<Category>? Categories { get; set; }
     public IEnumerable<Wallet>? Wallets { get; set; }
 }
