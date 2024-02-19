fcdeffb1f171b608610d493f027cb4ed939c59e8
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2024-02-19 23:47:14 +0300

    Add transaction dto

---
+++ Web/Dtos/TransactionDto.cs
@@ -0,0 +1,15 @@
+using Web.Models;
+
+namespace Web.Dtos;
+
+public class TransactionDto
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+    public string? Description { get; set; }
+    public Month? Month { get; set; }
+    public decimal? Cost { get; set; }
+    public int CategoryId { get; set; }
+    public Category? Category { get; set; }
+    public int WalletId { get; set; }
+}
