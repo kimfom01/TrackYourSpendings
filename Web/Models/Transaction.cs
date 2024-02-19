6bdd261ecb6073dff233044e0842fd2b1fc26fc0
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2024-02-19 23:56:38 +0300

    Add constraint to user id

--- Web/Models/Transaction.cs
+++ Web/Models/Transaction.cs
@@ -6,23 +6,19 @@ namespace Web.Models;
 public class Transaction
 {
     public int Id { get; set; }
-    
     [MaxLength(50)]
     public required string Name { get; set; }
-    
-    
     [MaxLength(300)]
     public string? Description { get; set; }
     public Month? Month { get; set; }
     public DateTime? Date { get; set; }
-
     [Precision(10, 2)]
     public decimal? Cost { get; set; }
     public int CategoryId { get; set; }
     public Category? Category { get; set; }
     public int WalletId { get; set; }
     public Wallet? Wallet { get; set; }
-    
+    [MaxLength(128)]
     public string? UserId { get; set; }
     public ApplicationUser? User { get; set; }
 }
