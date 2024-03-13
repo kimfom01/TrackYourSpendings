0bec407f824fbaa667c8a0b6a42c59ddfd00ed2a
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2024-03-13 16:20:10 +0300

    Fix hanging object issue

--- TrackYourSpendings.Web/wwwroot/js/qr.js
+++ TrackYourSpendings.Web/wwwroot/js/qr.js
@@ -1,9 +1,13 @@
 window.addEventListener("load", () => {
     const uri = document.getElementById("qrCodeData").getAttribute('data-url');
-    new QRCode(document.getElementById("qrCode"),
-        {
-            text: uri,
-            width: 150,
-            height: 150
-        });
+    try {
+        new QRCode(document.getElementById("qrCode"),
+            {
+                text: uri,
+                width: 150,
+                height: 150
+            });
+    } catch (ex){
+        console.error(ex.message);
+    }
 });
