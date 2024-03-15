b276fe0fde2129dde02987137610c78ef7d21bee
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2024-03-15 14:54:48 +0300

    Improve update script

--- update.sh
+++ update.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m'
+
+if [ $# -eq 0 ]; then
+	echo -e "${RED}No services provided!${NC}"
+	exit 1
+fi
+
+printf "You are updating the following services: ${GREEN}$@${NC}\n\n"
+
 git pull origin main
 
-docker compose pull && docker compose up -d
+docker compose pull && docker compose up -d $@
